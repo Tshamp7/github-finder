2962faf8877328d483fc424dbe324f5e5becb2cc
Tom Shamp <T.shamp7@gmail.com>
2020-10-17 11:27:14 -0700

    add proptypes

--- src/components/users/UserItem.jsx
+++ src/components/users/UserItem.jsx
@@ -1,24 +1,27 @@
-import React, { Component } from "react";
+import React from "react";
+import PropTypes from "prop-types";
 
-export default class UserItem extends Component {
-  render() {
-    const { avatar_url, login, html_url } = this.props.user;
-
-    return (
-      <div className='card text-center'>
-        <img
-          src={avatar_url}
-          alt='person'
-          className='round-img'
-          style={{ width: "60px" }}
-        />
-        <h3>{login}</h3>
-        <div>
-          <a href={html_url} className='btn btn-dark btn-sm my-1'>
-            More
-          </a>
-        </div>
+const UserItem = ({ user: { login, avatar_url, html_url } }) => {
+  return (
+    <div className='card text-center'>
+      <img
+        src={avatar_url}
+        alt='person'
+        className='round-img'
+        style={{ width: "60px" }}
+      />
+      <h3>{login}</h3>
+      <div>
+        <a href={html_url} className='btn btn-dark btn-sm my-1'>
+          More
+        </a>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+UserItem.propTypes = {
+  user: PropTypes.object.isRequired,
+};
+
+export default UserItem;
