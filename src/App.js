aad8912b4d2aa7f73e350b3ef0d387701b66add5
Tom Shamp <T.shamp7@gmail.com>
2020-10-20 09:06:07 -0700

    add home component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { Fragment } from "react";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
-import Users from "./components/users/Users";
-import Search from "./components/users/Search";
 import Alert from "./components/layout/Alert";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { About } from "./components/pages/About";
 import User from "./components/users/User";
 import GithubState from "./context/github/GithubState";
 import AlertState from "./context/alert/AlertState";
+import Home from "./components/pages/Home";
 
 const App = () => {
   return (
@@ -20,16 +19,7 @@ const App = () => {
             <div className='container'>
               <Alert />
               <Switch>
-                <Route
-                  exact
-                  path='/'
-                  render={props => (
-                    <Fragment>
-                      <Search />
-                      <Users />
-                    </Fragment>
-                  )}
-                />
+                <Route exact path='/' component={Home} />
                 <Route exact path='/about' component={About} />
                 <Route exact path='/user/:login' component={User} />
               </Switch>
